24b94a25c89ce8d88288293fdcc395a6dbdcab5e
Tanim <mdtasnimhossaintanim@gmail.com>
2025-07-27 00:17:54 +0600

    change something

--- index.js
+++ index.js
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-// document.getElementById("textAndHtml").innerHTML = `<p>Hello world</p>`;
-
-// fetch("https://jsonplaceholder.typicode.com/posts")
-// .then(response=>console.log(response.json()))
-// // .then(data=>console.log("data",data))
-
-// async function loadData() {
-//   try {
-//     let store = await fetch("https://jsonplaceholder.typicode.com/posts").then(
-//       (res) => res.json()
-//     );
-//     console.log(store)
-//     document.getElementById("textAndHtml").innerHTML= store.map((data)=>`
-//     <p style="color: red;">Title: ${data.title}</p>
-//     `).join("")
-     
-//   } catch (error) {
-//     console.log(error, "Failed");
-//   }
-// }
-// loadData();
-
-    
-    async function loadData() {
-  try {
-    let store = await fetch("https://jsonplaceholder.typicode.com/posts").then(
-      (res) => res.json()
-    );
-    console.log(store)
-    document.getElementById("textAndHtml").innerHTML = 
-    store.map((data)=>`<p style="color: white;">Title: ${data.title}  </p>` ).join("")
-     
-  } catch (error) {
-    console.log(error, "Failed");
-  }
-}
-loadData();
-=======
 // arrrow function
 
 const myStore = async () => {
@@ -63,4 +24,3 @@ const myStore = async () => {
 };
 
 myStore();
->>>>>>> 4b2b526 (first commit)
