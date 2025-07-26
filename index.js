a30bef6e49cc4d2dbf8f0d9802b4301ad38e5d5b
Tanim <mdtasnimhossaintanim@gmail.com>
2025-07-27 00:11:09 +0600

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // document.getElementById("textAndHtml").innerHTML = `<p>Hello world</p>`;
 
 // fetch("https://jsonplaceholder.typicode.com/posts")
@@ -35,3 +36,31 @@
   }
 }
 loadData();
+=======
+// arrrow function
+
+const myStore = async () => {
+  try {
+    let store = await fetch("https://fakestoreapi.com/products").then(
+      (response) => response.json()
+    );
+    console.log(store);
+
+    document.getElementById("card-container").innerHTML = store
+      .map(
+        (data) => `
+         <div class="card">
+        <img src="${data.image}" alt="" />
+        <h3>${data.title}</h3>
+        <p> price $ ${data.price}</p>
+      </div>
+        `
+      )
+      .join("");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+myStore();
+>>>>>>> 4b2b526 (first commit)
